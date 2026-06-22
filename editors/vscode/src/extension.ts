45ce100a098d24e439df15be6e62c706da53b917
Veesh Goldman <rabbiveesh@gmail.com>
2026-06-22 21:32:52 +0300

    chore: bump version to 0.5.2

--- editors/vscode/src/extension.ts
+++ editors/vscode/src/extension.ts
@@ -11,7 +11,7 @@ import {
 } from "vscode-languageclient/node";
 
 const REPO = "tree-sitter-perl/perl-tree-sitter-lsp";
-const VERSION = "0.5.1";
+const VERSION = "0.5.2";
 
 let client: LanguageClient | undefined;
 
